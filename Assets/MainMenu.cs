94c5f54209f883bca1f79bef5d0be7f2db87a909
Atackator <atackator05@gmail.com>
2025-04-04 19:48:04 +0300

    playbutton

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -5,6 +5,10 @@ using System.Collections.Generic;
 
 public class MainMenu : MonoBehaviour
 {
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
     public void QuitGame()
     {
         Application.Quit();
