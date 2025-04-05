3b84d9bc60707bb97ae83574133cb2c771ca8825
Denis-bit <denissarca@yahoo.com>
2025-04-06 02:44:22 +0300

    avem enemy

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int maxHealth = 100;
+    int currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("Enemy died");
+        // Handle enemy death (destroy, play animation, etc.)
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public Animator animator;
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayer;
+
+    public int attackDamage = 40;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Attack();
+        }
+    }
+    void Attack()
+    {
+        animator.SetTrigger("Attack");
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayer);
+        // foreach (Collider2D enemy in hitEnemies)
+        // {
+        //     // Damage the enemy
+        //     Debug.Log("Hit " + enemy.name);
+        //     enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
+        // }
+
+        Debug.Log("Attacking...");
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            Debug.Log("Found enemy: " + enemy.name);
+            Enemy enemyScript = enemy.GetComponent<Enemy>();
+            if (enemyScript != null)
+            {
+                enemyScript.TakeDamage(attackDamage);
+            }
+            else
+            {
+                Debug.LogWarning("Enemy script missing on " + enemy.name);
+            }
+        }
+
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+            return;
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+}
