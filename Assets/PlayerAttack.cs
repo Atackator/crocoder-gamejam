52d661f41a6cfc96c7931e25fc7465b503aaf38b
Atackator <atackator05@gmail.com>
2025-04-06 01:12:33 +0300

    sword_animation

---
+++ Assets/PlayerAttack.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public Animator animator;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Attack();
+        }       
+    }
+    void Attack()
+    {
+        animator.SetTrigger("Attack");
+    }
+}
